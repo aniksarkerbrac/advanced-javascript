63a0e97ac5f18cd11ada3d42660d05e00284d6aa
Anik Sarker <anik.brac.2016@gmail.com>
2020-08-25 16:02:41 +0600

    difference between true and false

---
+++ true-false.js
@@ -0,0 +1,10 @@
+//const age = 4;
+let age;
+console.log(age);
+if(age){ // here age will be always true whether value negative or positive. if age=0 then it shows false.
+    console.log("condition is true"); //same goes for string. if name = "" then it shows false.
+}
+else{
+    console.log("condition is false");
+}
+
