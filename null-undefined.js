2692bebb7f9ef47e8f52639ea7bec964a54e7486
Anik Sarker <anik.brac.2016@gmail.com>
2020-08-25 16:22:17 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,28 @@
+let age;
+console.log(age);
+//------------------
+
+function add(num1,num2){
+    console.log(num1 + num2);
+}
+const result = add(13,82);
+console.log(result);
+//------------------
+
+function add (num1,num2){
+    console.log(num1,num2);
+}
+const result = add(13);
+console.log(result);
+//------------------
+
+const person = {name: "smart dude", phone:35353};
+console.log(person.gf);
+//-------------------
+
+let fun = undefined;
+console.log(fun);
+//-------------------
+
+let ages = [2,4,4,5];
+console.log(ages[11]);
